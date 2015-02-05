ad36d9a2e48d0e8e6b469dda59f6cf71186b58b4
Stephen Garlick <sgarlick@taximagic.com>
2015-02-05 11:08:10 -0500

    more setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from distutils.core import setup
 
 setup(name='curbformation',
       version='0.1',
-      py_modules=['cf'],
+      scripts=[
+          'bin/cf',
+          'bin/sync',
+          'bin/bootstrap_env'
+      ],
+      url='https://github.com/ridecharge/curbformation',
+      packages=['cf']
 )
