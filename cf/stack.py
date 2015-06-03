f11277ca2d8a78854de8e7f386b14ab6551e63c2
Stephen Garlick <sgarlick@taximagic.com>
2015-06-03 10:50:24 -0400

    typo

--- cf/stack.py
+++ cf/stack.py
@@ -116,7 +116,7 @@ class StackService(object):
 
     def update_version_params(self, version, previous_version, stack):
         if not stack.options.skip_version_check:
-            cf.helpers.exist_when_version_not_found()
+            cf.helpers.exit_when_version_not_found(version)
         cf.helpers.add_version_param(version, previous_version, stack.params)
 
     def lock(self, stack_name):
